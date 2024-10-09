5d2dc6587e4848d3db3ee76380f16e101e789077
VoHuyChuong <chuong108304@donga.edu.vn>
2024-10-09 22:18:34 +0700

    Add files via upload

---
+++ Baitap.java
@@ -0,0 +1,59 @@
+package cauc;
+public class Baitap {
+    public static void main(String[] args) {
+        int [][][] arr = {{{5,2,3},{4,5,6,8},{7,7}},{{5,6,7,3},{8,9}}};
+        int cs1 = 0, cs2 = 0, cs3 = 0;
+        System.out.printf("Phần tử thứ 1: arr[%d][%d][%d] = %d", cs1, cs2, cs3, arr[cs1][cs2][cs3]);
+
+        cs3 = cs3 + 1;
+        System.out.printf("\nPhần tử thứ 2: arr[%d][%d][%d] = %d", cs1, cs2, cs3, arr[cs1][cs2][cs3]);
+
+        cs3 = cs3 + 1;
+        System.out.printf("\nPhần tử thứ 3: arr[%d][%d][%d] = %d", cs1, cs2, cs3, arr[cs1][cs2][cs3]);
+
+        cs2 = cs2 + 1; 
+        cs3 = 0; 
+        System.out.printf("\nPhần tử thứ 4: arr[%d][%d][%d] = %d", cs1, cs2, cs3, arr[cs1][cs2][cs3]);
+
+        cs3 = cs3 + 1;
+        System.out.printf("\nPhần tử thứ 5: arr[%d][%d][%d] = %d", cs1, cs2, cs3, arr[cs1][cs2][cs3]);
+
+        cs3 = cs3 + 1;
+        System.out.printf("\nPhần tử thứ 5: arr[%d][%d][%d] = %d", cs1, cs2, cs3, arr[cs1][cs2][cs3]);
+
+        cs3 = cs3 + 1;
+        System.out.printf("\nPhần tử thứ 5: arr[%d][%d][%d] = %d", cs1, cs2, cs3, arr[cs1][cs2][cs3]);
+
+        cs2 = cs2 +1;
+        cs3 = 0;
+        System.out.printf("\nPhần tử thứ 5: arr[%d][%d][%d] = %d", cs1, cs2, cs3, arr[cs1][cs2][cs3]);
+
+        cs3 = cs3 + 1;
+        System.out.printf("\nPhần tử thứ 5: arr[%d][%d][%d] = %d", cs1, cs2, cs3, arr[cs1][cs2][cs3]);
+
+        cs1 = cs1 + 1;
+        cs2 = 0;
+        cs3 = 0;
+        System.out.printf("\nPhần tử thứ 5: arr[%d][%d][%d] = %d", cs1, cs2, cs3, arr[cs1][cs2][cs3]);
+
+        cs3 = cs3 + 1;
+        System.out.printf("\nPhần tử thứ 5: arr[%d][%d][%d] = %d", cs1, cs2, cs3, arr[cs1][cs2][cs3]);
+
+        cs3 = cs3 + 1;
+        System.out.printf("\nPhần tử thứ 5: arr[%d][%d][%d] = %d", cs1, cs2, cs3, arr[cs1][cs2][cs3]);
+
+        cs3 = cs3 + 1;
+        System.out.printf("\nPhần tử thứ 5: arr[%d][%d][%d] = %d", cs1, cs2, cs3, arr[cs1][cs2][cs3]);
+
+        cs2 = cs2 + 1;
+        cs3 = 0;
+        System.out.printf("\nPhần tử thứ 5: arr[%d][%d][%d] = %d", cs1, cs2, cs3, arr[cs1][cs2][cs3]);
+
+        cs3 = cs3 + 1;
+        System.out.printf("\nPhần tử thứ 5: arr[%d][%d][%d] = %d", cs1, cs2, cs3, arr[cs1][cs2][cs3]);
+
+
+        
+        
+    }
+}
